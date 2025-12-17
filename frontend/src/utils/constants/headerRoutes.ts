029ab49da498011c275beaf0f295babf2dd66f62
TuThanh <tuthanh10825@gmail.com>
2025-12-17 17:00:22 +0700

    fix: header profile

--- frontend/src/utils/constants/headerRoutes.ts
+++ frontend/src/utils/constants/headerRoutes.ts
@@ -1,9 +1,8 @@
-import type { NavLink } from "../types/header"
+import type { NavLink } from "../types/header";
 
 export const headerNavLinks: NavLink[] = [
-  { label: 'Explore', to: { name: 'search' } },
-  { label: 'My Trips', to: { name: 'trip' } },
-  { label: 'Saved Place', to: { path: '/landing#saved' } },
-  { label: 'Forum', to: { name: 'forums-home' } },
-]
-
+  { label: "Explore", to: { name: "search" } },
+  { label: "My Trips", to: { name: "trip" } },
+  { label: "Saved Place", to: { path: "/landing#saved" } },
+  { label: "Forum", to: { name: "forums-home" } },
+];
